36e7452010551ac3d25780852378debe2413f207
Nolin McFarland <nolin.mcfarland@icloud.com>
2022-02-19 16:18:44 -0500

    Initial Commit

---
+++ EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by Nolin McFarland on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
